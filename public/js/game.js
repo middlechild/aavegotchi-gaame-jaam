3f8cab40962d0a9ac72c132cba598b4e4685fbd1
middlechild <hello@natacha.me>
2021-09-05 01:21:56 -0400

    Progress

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,7 @@
 function Game() {
-    let that, game, player, cursors;
-    let platforms, background, ground, ceiling, walls;
+    let that, game, player, cursors, spacebar;
+    let paused, playing;
+    let platforms, background, ground, ceiling, walls, forces;
     let scoreBox, itemsBox, playButton;
     let soundSend, soundOops, soundClick, soundPortal;
     let respawnTime = 0;
@@ -27,7 +28,7 @@ function Game() {
                 default: 'arcade',
                 arcade: {
                     gravity: { y: 300 },
-                    debug: true
+                    debug: false
                 }
             },
             scene: {
@@ -39,8 +40,8 @@ function Game() {
         // Game settings
         settings: {
             velocity: 10,
-            jumpHeight: -300,
-            gravityY: 500
+            jumpHeight: -200,
+            gravityY: 300
         }
     }
 
@@ -60,6 +61,7 @@ function Game() {
     async function setPlayerAttributes() {
         // Get NFTs data (Only Aavegotchis)
         const query = new Moralis.Query("PolygonNFTOwners") || Moralis.Query("EthNFTOwners");
+        query.equalTo("owner_of", playerAttributes.ethAddress);
         query.equalTo("name", "Aavegotchi");
         const results = await query.find();
         console.log("Successfully retrieved " + results.length + " results.");
@@ -87,29 +89,20 @@ function Game() {
             .setGravityY(config.settings.gravityY)
             .refreshBody();
         player.setBounce(0.3);
+        player.setDepth(100);
         that.physics.add.collider(player, [platforms, walls]);
-        // that.physics.add.collider(player, walls);
     }
 
     // Walls, enemies, and items
     function addWall() {
-        const count = Math.floor(Math.random() * 7) + 1;
-        const distance = Phaser.Math.Between(600, 900);
-
-        // let wall = walls.create(1080 + distance, 720, "wall").setOrigin(0, 0);
-        // wall.body.offset.y = +10;
-
-        let wall = that.add.image(1080 + distance, 360, "wall");
+        const distance = Phaser.Math.Between(540, 920);
+        let wall = that.add.image(800 + distance, 360, "wall");
         walls.add(wall);
 
-        // if (count > 6) {
-        //
-        // } else {
-        //     wall = this.walls.create(1280 + distance, 720, "wall").setOrigin(0, 1);
-        //     wall.body.offset.y = +10;
-        // }
-
-        // wall.setImmovable();
+        const offset = Phaser.Math.Between(200, 430);
+        let force = that.physics.add.image(802 + distance, offset, "force");
+        force.setDepth(101);
+        forces.add(force);
     }
 
     // preload assets
@@ -119,6 +112,7 @@ function Game() {
         this.load.image('ground', 'assets/tiles/tile30.png');
         this.load.image('ceiling', 'assets/tiles/tile40.png');
         this.load.image('wall', 'assets/tiles/tile26.png');
+        this.load.image('force', 'assets/tiles/force60.png');
 
         this.load.audio('send', 'assets/sounds/send.mp3');
         this.load.audio('oops', 'assets/sounds/oops.mp3');
@@ -151,8 +145,10 @@ function Game() {
         ceiling = this.add.tileSprite(640,56,1280,112,"ceiling");
         platforms.add(ceiling);
 
-        walls = this.physics.add.group();
+        walls = this.physics.add.group({runChildUpdate: true});
         that.physics.add.collider(platforms, walls);
+
+        forces = this.physics.add.group({runChildUpdate: true, allowGravity: false});
 
         soundSend = this.sound.add('send', {volume: 0.2});
         soundOops = this.sound.add('oops', {volume: 0.2});
@@ -168,18 +164,23 @@ function Game() {
         itemsBox.setShadow(16, 16, 'rgba(0,0,0,1)', 32);
 
         cursors = this.input.keyboard.createCursorKeys();
+        spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     // 60 fps updates
     async function update (time, delta) {
-        if(!player)
+        if(!player || paused)
             return;
-    return;
+
         ground.tilePositionX += config.settings.velocity;
         ceiling.tilePositionX += config.settings.velocity;
         background.tilePositionX += 2;
 
         Phaser.Actions.Call(walls.getChildren(), function(e) {
+            e.x -= config.settings.velocity;
+        })
+
+        Phaser.Actions.Call(forces.getChildren(), function(e) {
             e.x -= config.settings.velocity;
         })
 
@@ -191,38 +192,32 @@ function Game() {
         }
 
         if (cursors.left.isDown) {
-            player.setVelocityX(-config.settings.velocity/2);
-            // ground.tilePositionX -= config.settings.velocity;
-            // ceiling.tilePositionX -= config.settings.velocity;
-            // background.tilePositionX -= 2;
-
-            Phaser.Actions.Call(walls.getChildren(), function(e) {
-                e.x += config.settings.velocity;
-            })
-
-        } else if (cursors.right.isDown) {
-            player.setVelocityX(config.settings.velocity/2);
-            // ground.tilePositionX += config.settings.velocity;
-            // ceiling.tilePositionX += config.settings.velocity;
-            // background.tilePositionX += 2;
-
+            let what = that.physics.world.overlap(player, walls)
+            console.log(what);
+            // player.setVelocityX(-config.settings.velocity/2);
+            //
             // Phaser.Actions.Call(walls.getChildren(), function(e) {
-            //     e.x -= config.settings.velocity;
+            //     e.x += config.settings.velocity;
             // })
 
-            // respawnTime += delta * config.settings.velocity * 0.08;
-            // if (respawnTime >= 1500) {
-            //     addWall();
-            //     console.log(respawnTime);
-            //     respawnTime = 0;
-            // }
+        // } else if (cursors.right.isDown) {
+        //     player.setVelocityX(config.settings.velocity/2);
+        // } else {
+        //     player.setVelocityX(0);
 
-        } else {
-            player.setVelocityX(0);
         }
 
-        if (cursors.up.isDown && player.body.touching.down) {
+        if (cursors.up.isDown) {
             player.setVelocityY(config.settings.jumpHeight);
+        }
+
+        if (cursors.down.isDown) {
+            player.setVelocityY(-config.settings.jumpHeight);
+        }
+
+        // Quick pause permanently - change this
+        if (spacebar && spacebar.isDown) {
+            paused = true;
         }
     }
 }
